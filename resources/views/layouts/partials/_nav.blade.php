45e1cafdd9e6135baaa429bde3ace53429b569eb
Makenley <makenley1@gmail.com>
2018-03-23 04:12:17 -0500

    update app.css, style, app.js, app.blade,footer.blade,nav.blade,welcome.blade, package-lock.json,public/mix-manifest.json

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -6,7 +6,7 @@
       </div>
        
       <div class="col-lg-3 offset-lg-5">
-        <div class="social-icon"> <a target="_blank" href="#" class=" fa fa-facebook"></a> <a target="_blank" href="#" class=" fa fa-twitter"></a> <a target="_blank" href="#" class=" fa fa-google-plus"></a> <a target="_blank" href="#" class=" fa fa-linkedin"></a> <a target="_blank" href="#" class=" fa fa-youtube"></a> <a target="_blank" href="#" class=" fa fa-vimeo-square"></a> </div>
+        <div class="social-icon"> <a target="_blank" href="#" class=" fa fa-facebook"></a> <a target="_blank" href="#" class=" fa fa-twitter"></a> <a target="_blank" href="#" class=" fa fa-google-plus"></a> <a target="_blank" href="#" class=" fa fa-linkedin"></a> <a target="_blank" href="#" class=" fa fa-youtube"></a> </div>
 
         
       </div>
