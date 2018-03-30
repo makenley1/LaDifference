17255581e14a4ed5766319e26fba42fd618f921e
Makenley <makenley1@gmail.com>
2018-03-30 13:08:22 -0500

    update

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -55,7 +55,7 @@
 </div>
 
 <section class="top-nav">
-  <nav class="navbar navbar-expand-lg py-0 navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg py-0 navbar-light">
     <div class="container">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
