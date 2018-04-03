62a8926c697c14238f206d6ca98615766a326e28
Makenley <makenley1@gmail.com>
2018-04-03 11:41:44 -0500

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
     @yield('content')
 
     @include('layouts/partials/_footer')
-    <a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+    
     <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -64,9 +64,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="nav navbar-nav" style="width: 77%;">
           <li class="nav-item {{Request::is('/') ? 'active' : ''}}"> <a class="nav-link" href="{{url('/')}}">Accueil</a> </li>
-          <li class="nav-item {{Request::is('/contact') ? 'active' : ''}}"> <a class="nav-link" href="{{url('contact')}}">Contact </a> </li>
-          <li class="nav-item {{Request::is('/aide') ? 'active' : ''}}"> <a class="nav-link" href="#">Aide</a> </li>
-          <li class="nav-item {{Request::is('/business') ? 'active' : ''}}"> <a class="nav-link" href="#">Business</a> </li>
+          <li class="nav-item {{Request::is('contact') ? 'active' : ''}}"> <a class="nav-link" href="{{url('contact')}}">Contact </a> </li>
+          <li class="nav-item {{Request::is('aide') ? 'active' : ''}}"> <a class="nav-link" href="#">Aide</a> </li>
+          <li class="nav-item {{Request::is('business') ? 'active' : ''}}"> <a class="nav-link" href="#">Business</a> </li>
         </ul>
         <form class="ml-auto">
           <div class="search">
